7f3f336afa062f747641fbe8f40593ea00be44b9
MarcosSerpa748 <msrserpa@gmail.com>
2026-05-25 13:08:30 -0300

    initial commit

--- app/src/main/java/com/example/treino/viewmodel/TelaTesteViewModel.kt
+++ app/src/main/java/com/example/treino/viewmodel/TelaTesteViewModel.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -18,23 +18,27 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TelaTesteViewModel@Inject constructor(private val savedStateHandle: SavedStateHandle):ViewModel(){
+    val valoresPassados = savedStateHandle.toRoute<TelaTesteRoute>()
+    val valorConvertido = valoresPassados.tamanho.toIntOrNull()
 
     private val _navegar = Channel<Unit>()
     val navegar = _navegar.receiveAsFlow()
 
     private val _transformar = MutableStateFlow(false)
-    private val _valorRecebido = MutableStateFlow<Int?>(null)
+    private val _valorRecebido = MutableStateFlow(valorConvertido)
 
-    val valoresPassados = savedStateHandle.toRoute<TelaTesteRoute>()
-    val valorConvertido = valoresPassados.tamanho.toIntOrNull()
 
-    val uiState = _transformar.map {
-        TelaTesteUIState(transformar = it, valorRecebido = valorConvertido)
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = TelaTesteUIState()
-        )
+    val uiState = combine(
+        _transformar,
+        _valorRecebido
+    ){tranformar,valorRecebido ->
+        TelaTesteUIState(transformar = tranformar, valorRecebido = valorRecebido)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = TelaTesteUIState()
+    )
+
 
     fun alterarEstado(valor: Boolean){
         _transformar.value = valor
